fc8eca807676dfd43f7b6fee0870caa93ec7eebc
ddorn <diego.dorn@free.fr>
2018-12-20 10:44:57 +0100

    group tokens together

--- triptopoland.cpp
+++ triptopoland.cpp
@@ -3,19 +3,28 @@
 #include <string>
 
 using namespace std;
+
 constexpr char MINUS_SIGN = '_';  // True minus sign, for unary minus
+
+using Tokens = vector<string>;
 
 void removeSpace(string& s);
 void fixUnaryMinus(string& s);
+bool isAlpha(string s);
+Tokens tokenize(string s);
+string popToken(string& s);
+
 
 int main() {
-	string math("-ln(7) * 1  +2*3 - (-5-6)");
+	string math("-ln(10 + 7) * 1  +2*3 - (-5-6)");
 	cout << math << endl;
 
-	removeSpace(math);
-	fixUnaryMinus(math);
+	Tokens t = tokenize(math);
 
-	cout << math << endl;
+	for (auto tok : t) {
+		cout << tok << endl;
+	}
+
 	return 0;
 }
 
@@ -36,4 +45,50 @@ void fixUnaryMinus(string& s) {
 			s[i] = MINUS_SIGN;
 		}
 	}
+}
+
+string popToken(string& s) {
+	// The purpose of this function is to group together tokens that ar more than one char long, like integers or functions names
+	string token(1, s[0]);
+
+	// if it is a function
+	auto firstParenthesis = s.find('(');
+	if (firstParenthesis != 0 && firstParenthesis != string::npos) {
+		if (isAlpha(s.substr(0, firstParenthesis))) {
+			token = s.substr(0, firstParenthesis + 1);
+		}
+	}
+
+	// if it is anumber
+	if (isdigit(s[0])) {
+		token = "";
+		int pos(0);
+		while (isdigit(s[pos])) {
+			token += s[pos];
+			++pos;
+		}
+	}
+
+	s.erase(0, token.size());
+	return token;
+}
+
+Tokens tokenize(string s) {
+	Tokens tokens;
+
+	removeSpace(s);
+	fixUnaryMinus(s);
+
+	while (not s.empty()) {
+		tokens.push_back(popToken(s));
+	}
+
+	return tokens;
+}
+
+bool isAlpha(string s) {
+	for (auto c : s) {
+		if (not isalpha(c)) return false;
+	}
+	return true;
 }
